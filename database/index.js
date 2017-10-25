53bc11715bd7ecc1ed9e5c04a30631b62b2338b9
Joe Liotta <joe.liotta00@gmail.com>
2017-10-25 09:36:28 -0700

    Set up sequelize connection to db publisher_dashboard

---
+++ database/index.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('publisher_dashboard', 'tweeter', 'YGhhbv6', {
+	host: 'localhost',
+	dialect: 'postgres'
+});
+
+sequelize
+	.authenticate()
+	.then(() => {
+		console.log('Connection has been successfully established');
+	})
+	.catch((err) => {
+		console.error('Unable to connect to the database:', err);
+	});
+
+	module.exports = sequelize;
