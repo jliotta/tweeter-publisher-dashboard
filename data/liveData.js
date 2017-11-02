c1f3e8e544ec4c72f0fbf6178016f5ce0c3eb75a
Joe Liotta <joe.liotta00@gmail.com>
2017-11-02 16:59:40 -0700

    Update live data and tweet data generators to reflect final format

--- data/liveData.js
+++ data/liveData.js
@@ -1,32 +1,100 @@
 const AWS = require('aws-sdk');
+const grammar = require('./grammar.js');
+const uuidv4 = require('uuid/v4');
+
+const tweetIds = require('./tweets.js')
 
 AWS.config.loadFromPath('./config.json');
 
 const sqs = new AWS.SQS({apiVersion: '2012-11-05'});
-
 const queueUrl = 'https://sqs.us-west-1.amazonaws.com/748557891852/Testing';
 
-var tweet = { 
-  tweetId: 1,
-  userId: 151754,
-  message: 'desperately demolished a dreamy dinosaur #bravenewworld',
-  createdAt: 'Sat Sep 16 2017 16:02:45 GMT-0700 (PDT)',
-  impressions: 33916002,
-  views: 32668,
-  likes: 290600,
-  replies: 2176,
-  retweets: 43439 
+var randomElement = function(array){
+  var randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
 };
 
-const params = {
-  MessageBody: JSON.stringify(tweet),
-  QueueUrl: queueUrl
+var messageGenerator = function() {
+  var message = 
+    [randomElement(grammar.opening), randomElement(grammar.verbs), randomElement(grammar.objects), 
+    randomElement(grammar.nouns), randomElement(grammar.tags)];
+	
+	if (grammar.properNouns.includes(message[3]) && message[2][0] !== 'a') {
+		message.splice(2, 1);
+	}
+
+	return message.join(' ').trim();
 };
 
-sqs.sendMessage(params, (err, data) => {
-  if (err) {
-    console.log('Sending Error:', err);
-  } else {
-    console.log('Sending Successful:', data);
-  }
-});
+var numberGenerator = function(n) {
+	return Math.floor(Math.random() * n);
+};
+
+var dateGenerator = function() {
+  // Only produce dates from August - October 2017
+  return Math.floor(Math.random() * (1509519599000 - 1501570800000) + 1501570800000).toString();
+};
+
+var userGenerator = function() {
+	return Math.floor(Math.random() * 699999 + 1);
+};
+
+var metricsGenerator = function() {
+  const params = {
+    MessageAttributes: {
+      userId: {
+        DataType: 'String',
+        StringValue: userGenerator().toString()
+      },
+      tweetId: {
+        DataType: 'String',
+        StringValue: '00042b77-dc78-47df-97e3-b10a482586cb'
+      },
+      type: {
+        DataType: 'String',
+        StringValue: randomElement(['impression'])
+      }
+    },
+    MessageBody: 'Metric',
+    QueueUrl: queueUrl
+  };
+
+  if (params.MessageAttributes.type.StringValue === 'reply') {
+    params.MessageAttributes.id = {
+      DataType: 'String',
+      StringValue: uuidv4()
+    };
+
+    params.MessageBody = messageGenerator();
+
+  } else if (params.MessageAttributes.type.StringValue === 'retweet') {
+    params.MessageAttributes.id = {
+      DataType: 'String',
+      StringValue: uuidv4()
+    };
+  } else if (params.MessageAttributes.type.StringValue === 'original') {
+    params.MessageBody = messageGenerator();
+    params.MessageAttributes.id = {
+      DataType: 'String',
+      StringValue: uuidv4()
+    };
+  } 
+
+  sqs.sendMessage(params, (err, data) => {
+    if (err) {
+      console.log('Sending Error:', err);
+    } else {
+      console.log('Sending Successful:', data);
+    }
+  });
+}
+
+var count = 0;
+
+while (count < 10000) {
+  metricsGenerator();
+  count++;
+}
+
+
+
--- data/tweetDataGenerator.js
+++ data/tweetDataGenerator.js
@@ -1,11 +1,19 @@
+const AWS = require('aws-sdk');
 const faker = require('faker');
-const publishers = require('./publishers.js');
-const elasticsearch = require('elasticsearch');
 const grammar = require('./grammar.js');
+const uuidv4 = require('uuid/v4');
+const csv = require('fast-csv');
+const fs = require('file-system');
+const ws = fs.createWriteStream('tweets.csv');
 
-const client = new elasticsearch.Client({
-  host: 'localhost:9200'
-});
+const tweetIds = require('./tweets.js');
+
+AWS.config.loadFromPath('./config.json');
+
+const sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+const queueUrl = 'https://sqs.us-west-1.amazonaws.com/748557891852/Testing';
+
+var tweets = [];
 
 var randomElement = function(array){
   var randomIndex = Math.floor(Math.random() * array.length);
@@ -29,20 +37,8 @@ var numberGenerator = function(n) {
 };
 
 var dateGenerator = function() {
-	// Only produce dates from August - October 2017
-	var day = numberGenerator(32);
-	while (day === 0) {
-		day = numberGenerator(32);
-	}
-
-	var months = [9, 8, 7];
-	var randomMonth = months[numberGenerator(months.length)];
-	var randomDay = day;
-	var randomHour = numberGenerator(24);
-	var randomMinute = numberGenerator(60);
-	var randomSecond = numberGenerator(60);
-
-	return new Date(2017, randomMonth, randomDay, randomHour, randomMinute, randomSecond).toISOString();
+  // Only produce dates from August - October 2017
+  return Math.floor(Math.random() * (1509519599000 - 1501570800000) + 1501570800000).toString();
 };
 
 var userGenerator = function() {
@@ -51,58 +47,43 @@ var userGenerator = function() {
 	var randomNumber = numberGenerator(100);
 
 	if (randomNumber <= 7) {
-    user = randomElement(publishers);
+    user = numberGenerator(200000) + 1;
 	} else {
-		user = numberGenerator(500000);
+		user = numberGenerator(700000);
 		while (user === 0) {
-			user = numberGenerator(500000);
+			user = numberGenerator(700000);
 		}
 	}
 	return user;
 };
 
-var tweetGenerator = function(id) {
-  var counter = 0;
-  var tweets = [];
+var tweetGenerator = function() {
+  var impressions = numberGenerator(1000000);
+  var views = Math.floor((impressions / 10) * Math.random());
+  var likes = Math.floor((impressions / 8) * Math.random());
+  var replies = Math.floor((impressions / 500) * Math.random());
+  var retweets = Math.floor((impressions / 250) * Math.random());
 
-  while (counter < 250000) {
-    var impressions = numberGenerator(1000000);
-    var views = Math.floor((impressions / 10) * Math.random());
-    var likes = Math.floor((impressions / 8) * Math.random());
-    var replies = Math.floor((impressions / 500) * Math.random());
-    var retweets = Math.floor((impressions / 250) * Math.random());
-  
-    var tweet = {
-      tweetId: id,
-      userId: userGenerator(),
-      message: messageGenerator(),
-      createdAt: dateGenerator(),
-      impressions: impressions,
-      views: views,
-      likes: likes,
-      replies: replies,
-      retweets: retweets
-    }
-    tweets.push({ 
-      index:  { _index: 'tweet', _type: 'all', _id: id } 
-    });
-    tweets.push(tweet);
-    id++;
-    counter++;
-  }
-	
-  return client.bulk({
-    body: tweets
-  }).catch(err => {
-    console.log('Bulk Insertion Error:', err);
-  });
-};
-
-// Generate 10 million tweets and bulk insert into Elasticsearch
-var batchTweets = async function() {
-  for (var i = 1; i < 10000000; i = i + 250000) {
-    await tweetGenerator(i);
-  }
+  var tweet = {
+    tweetId: uuidv4().toString(),
+    userId: userGenerator().toString(),
+    message: messageGenerator(),
+    createdAt: dateGenerator(),
+    impressions: impressions,
+    views: views,
+    likes: likes,
+    replies: replies,
+    retweets: retweets,
+    type: 'original'
+  };
+  tweets.push(tweet);
 }
 
-batchTweets();
+var count = 0;
+
+while (count < 50000) {
+  tweetGenerator();
+  count++;
+}
+
+csv.write(tweets, {headers:false}).pipe(ws);
