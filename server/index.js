b94f668f05bcc0295ead2abf54af7e2f9ce3743f
Joe Liotta <joe.liotta00@gmail.com>
2017-11-06 19:34:10 -0800

    Set up node cluster and cron worker to send tweets to queue

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const AWS = require('aws-sdk');
+const cluster = require('cluster');
+const Cron = require('cron').CronJob;
 const db = require('./dbQueries.js');
 const formatMetric = require('./helpers.js').formatMetric;
 const formatTweet = require('./helpers.js').formatTweet;
@@ -7,7 +9,8 @@ const bulkStorage = require('./bulkStorage.js');
 AWS.config.loadFromPath('./config.json');
 
 const sqs = new AWS.SQS({apiVersion: '2012-11-05'});
-const queueUrl = 'https://sqs.us-west-1.amazonaws.com/748557891852/Testing';
+const queueUrl = require('../config.js').url;
+const sendUrl = require('../config.js').sendUrl;
 
 const params = {
   QueueUrl: queueUrl,
@@ -39,7 +42,7 @@ var pollQueue = () => {
                 bulkStorage[type].push(formatMetric(message));
                 db[type](message.MessageAttributes.tweet_id.StringValue);
 
-                if (bulkStorage[type].length === 500) {
+                if (bulkStorage[type].length === 1500) {
                   var tweets = bulkStorage['tweet'];
                   var items = bulkStorage[type];
                   bulkStorage['tweets'] = [];
@@ -55,4 +58,38 @@ var pollQueue = () => {
   }) 
 };
 
-setInterval(pollQueue, 10);
+var sendTweets = new Cron('0 0 * * *', () => {
+  var today = new Date().valueOf();
+  var pastDay = today - 86400000;
+
+  db['getTodaysTweets'](pastDay)
+  .then(results => {
+    results[0].forEach(tweet => {
+      sqs.sendMessage({MessageBody: JSON.stringify(tweet), QueueUrl: sendUrl}, (err, data) => {
+        if (err) {
+          console.log('Sending Error:', err);
+        } else {
+          console.log('Sent Message:', data);
+        }
+      });
+    });
+  });
+});
+
+if (cluster.isMaster) {
+  var numWorkers = require('os').cpus().length;
+  for (var i = 0; i < numWorkers; i++) {
+    cluster.fork();
+  }
+  cluster.on('online', (worker) => {
+    console.log('Worker ' + worker.process.pid + ' is online');
+  });
+  cluster.on('exit', (worker, code, signal) => {
+    console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ' and signal: ' + signal);
+    console.log('Starting a new worker');
+    cluster.fork();
+  });
+  sendTweets.start();
+} else {
+  setInterval(pollQueue, 10);
+}
